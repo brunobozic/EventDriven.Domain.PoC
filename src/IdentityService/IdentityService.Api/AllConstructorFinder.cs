93487514237bfcde34318396a6269d6d170f7c6d
Bruno Božić <bruno.bozic@gmail.com>
2023-12-11 23:45:41 +0100

    Rolled back MediatR update to the older version, the new version is all fucked up, need to talk to Bogard *AGAIN* to see what need to be changed.

---
+++ src/IdentityService/IdentityService.Api/AllConstructorFinder.cs
@@ -0,0 +1,31 @@
+﻿using Autofac.Core.Activators.Reflection;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System;
+using System.Linq;
+
+namespace EventDriven.Domain.PoC.Api.Rest
+{
+    public class AllConstructorFinder : IConstructorFinder
+    {
+        private static readonly ConcurrentDictionary<Type, ConstructorInfo[]> Cache =
+            new();
+
+        public ConstructorInfo[] FindConstructors(Type targetType)
+        {
+            var result = Cache.GetOrAdd(targetType,
+                t => t.GetTypeInfo().DeclaredConstructors.ToArray());
+
+            try
+            {
+                var temp = result.Length > 0 ? result : throw new NoConstructorsFoundException(targetType, this);
+
+                return temp;
+            }
+            catch (Exception)
+            {
+                throw new NoConstructorsFoundException(targetType, this);
+            }
+        }
+    }
+}
